fbce8a3502a264e7029f7f0cbf63823168f5414b
Вячеслав <mirsonov.slava@mail.ru>
2019-10-02 22:21:43 +0300

    one

---
+++ TestApp2/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp2
+{
+	class Class1
+	{
+		void Print()
+		{
+			Console.WriteLine("11");
+		}
+	}
+}
